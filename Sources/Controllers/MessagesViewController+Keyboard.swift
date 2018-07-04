0f22a30ecea28dca26bfe8896fd038d4aab47c09
Phanuwat Yoksiri <ibugattiz@aol.com>
2018-07-04 17:10:05 +0700

    update keyboard

--- Sources/Controllers/MessagesViewController+Keyboard.swift
+++ Sources/Controllers/MessagesViewController+Keyboard.swift
@@ -65,8 +65,10 @@ extension MessagesViewController {
             // Hardware keyboard is found
             if view.frame.size.height - keyboardEndFrame.origin.y == 0 {
             } else {
-                messageCollectionViewBottomInset = view.frame.size.height - keyboardEndFrame.origin.y - iPhoneXBottomInset - messageBarHeight
-                beforeCollectionViewBottomInset = messageCollectionViewBottomInset
+                let afterBottomInset = keyboardEndFrame.height > keyboardOffsetFrame.height ? (keyboardEndFrame.height - iPhoneXBottomInset) : keyboardOffsetFrame.height
+                let differenceOfBottomInset = afterBottomInset - beforeCollectionViewBottomInset
+                let contentOffset = CGPoint(x: messagesCollectionView.contentOffset.x, y: messagesCollectionView.contentOffset.y + differenceOfBottomInset)
+                messagesCollectionView.setContentOffset(contentOffset, animated: false)
             }
         } else {
             //Software keyboard is found
